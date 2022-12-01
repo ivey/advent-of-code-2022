fc130067de4ca5b20b95a9ca6878c6c37e095a67
Michael D. Ivey <ivey@gweezlebur.com>
2022-12-01 13:43:46 -0600

    cleanup

--- day01/test.cc
+++ day01/test.cc
@@ -4,12 +4,12 @@
 #include <sstream>
 
 TEST(Part1Test, Test1) {
-    std::vector<std::pair<std::string, uint32_t>> inputs = {
-        {"", 0},
-        {"1000", 1000},
-        {"1000\n2000", 3000},
-        {"1000\n2000\n\n5000", 5000},
-        {"1000\n2000\n\n5000\n\n1100\n1200\n1300\n1400\n1500", 6500},
+    std::vector <std::pair<std::string, uint32_t>> inputs = {
+            {"",                                                   0},
+            {"1000",                                               1000},
+            {"1000\n2000",                                         3000},
+            {"1000\n2000\n\n5000",                                 5000},
+            {"1000\n2000\n\n5000\n\n1100\n1200\n1300\n1400\n1500", 6500},
     };
 
     for (auto &i : inputs) {
@@ -19,10 +19,10 @@ TEST(Part1Test, Test1) {
 }
 
 TEST(Part2Test, Test1) {
-    std::vector<std::pair<std::string, uint32_t>> inputs = {
-        {"1\n\n2\n\n3", 6},
-        {"1000\n2000\n\n5000\n\n1100\n1200\n1300\n1400\n1500", 14500},
-        {"1\n2\n3\n\n100\n\n200\n300\n\n6000\n\n10000\n\n10000\n\n10000", 30000},
+    std::vector <std::pair<std::string, uint32_t>> inputs = {
+            {"1\n\n2\n\n3",                                                   6},
+            {"1000\n2000\n\n5000\n\n1100\n1200\n1300\n1400\n1500",            14500},
+            {"1\n2\n3\n\n100\n\n200\n300\n\n6000\n\n10000\n\n10000\n\n10000", 30000},
     };
 
     for (auto &i : inputs) {
